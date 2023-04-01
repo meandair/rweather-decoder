c9b7452557699e417e1051ad73e5f514868f4d31
Filip Svabik <fsv@meandair.com>
2023-04-01 18:41:58 +0200

    Add tests for plain daytime, implements #1

--- tests/it_metar.rs
+++ tests/it_metar.rs
@@ -6,25 +6,30 @@ use anyhow::Result;
 use rweather_decoder::metar::Metar;
 use tempfile::NamedTempFile;
 
-fn run_decode_metar(input: &PathBuf, output: &PathBuf) -> Result<()> {
+fn run_decode_metar(input: &PathBuf, output: &PathBuf, file_format: &str) -> Result<()> {
     let binary_path = env!("CARGO_BIN_EXE_decode-metar");
 
     let status = Command::new(&binary_path)
-        .args(&[input, output])
+        .args(&[
+            input.as_os_str().to_str().unwrap(),
+            output.as_os_str().to_str().unwrap(),
+            "-f",
+            file_format
+        ])
         .status()?;
     assert!(status.success());
 
     Ok(())
 }
 
-fn it_metar_template(input: &str, given_output: &str) -> Result<()> {
+fn it_metar_template(input: &str, given_output: &str, file_format: &str) -> Result<()> {
     let input_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests").join("data").join("metar").join(input);
     let given_output_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests").join("data").join("metar").join(given_output);
 
     let test_output = NamedTempFile::new_in(env!("CARGO_TARGET_TMPDIR"))?.into_temp_path();
     let test_output_path = test_output.to_path_buf();
 
-    run_decode_metar(&input_path, &test_output_path)?;
+    run_decode_metar(&input_path, &test_output_path, file_format)?;
 
     let file = File::open(&test_output_path)?;
     let buf_reader = BufReader::new(file);
@@ -44,6 +49,11 @@ fn it_metar_template(input: &str, given_output: &str) -> Result<()> {
 }
 
 #[test]
+fn it_metar_daytime() -> Result<()> {
+    it_metar_template("it_daytime_input.txt", "it_daytime_output.json", "plain")
+}
+
+#[test]
 fn it_metar_header() -> Result<()> {
-    it_metar_template("it_header_input.txt", "it_header_output.json")
+    it_metar_template("it_header_input.txt", "it_header_output.json", "noaa-metar-cycles")
 }
